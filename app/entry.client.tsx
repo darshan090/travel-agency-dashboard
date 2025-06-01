a56ef38457e9edfa2e207cf1a764f02e9988e701
Darshan <darshanjariwala090@gmail.com>
2025-06-01 09:34:24 +0530

    logout route

---
+++ app/entry.client.tsx
@@ -0,0 +1,33 @@
+import * as Sentry from "@sentry/react-router";
+ import { startTransition, StrictMode } from "react";
+ import { hydrateRoot } from "react-dom/client";
+ import { HydratedRouter } from "react-router/dom";
+
+Sentry.init({
+ dsn: "https://d68fe04722d3e53c437e66b1b2d9118a@o4509400761630720.ingest.us.sentry.io/4509400763727872",
+ 
+ // Adds request headers and IP for users, for more info visit:
+ // https://docs.sentry.io/platforms/javascript/guides/react-router/configuration/options/#sendDefaultPii
+ sendDefaultPii: true,
+ 
+ integrations: [
+   Sentry.reactRouterTracingIntegration(),
+   Sentry.replayIntegration(),
+ ],
+ tracesSampleRate: 1.0, //  Capture 100% of the transactions
+ // Set `tracePropagationTargets` to declare which URL(s) should have trace propagation enabled
+ tracePropagationTargets: [/^\//, /^https:\/\/yourserver\.io\/api/],
+ // Capture Replay for 10% of all sessions,
+ // plus 100% of sessions with an error
+ replaysSessionSampleRate: 0.1,
+ replaysOnErrorSampleRate: 1.0,
+});
+
+startTransition(() => {
+  hydrateRoot(
+    document,
+    <StrictMode>
+      <HydratedRouter />
+    </StrictMode>
+  );
+});
